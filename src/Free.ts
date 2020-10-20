016f92b8b0208cfa4bc949dd7223e52ebcd3e9ce
Jason Santiago <jasonsantiagobutler@gmail.com>
2020-10-19 23:03:50 -0300

    chainer correct signature

--- src/Free.ts
+++ src/Free.ts
@@ -1,13 +1,13 @@
-export const FREE_URI = 'Free'
+export const FREE_URI = 'Free';
 
-export type FREE_URI = typeof FREE_URI
+export type FREE_URI = typeof FREE_URI;
 export abstract class Free<Eff = any, A = any> {
   // note, if you put the type Eff inside the record, then typescript won't let you assign Free<Database, any> to Free<Async, any>
 
-  Eff!: Eff
+  Eff!: Eff;
   A!: A;
   *[Symbol.iterator]() {
-    return (yield this) as A
+    return (yield this) as A;
   }
   //   _Eff!: Eff
   //   private _A!: A
@@ -16,12 +16,12 @@ export abstract class Free<Eff = any, A = any> {
   //     return new Chain(this, chainer)
   //   }
   abstract chain<B, Eff2>(
-    chainer: (value: A) => Free<Eff2, B>
-  ): Free<Eff | Eff2, B>
+    chainer: (value: A) => Free<Eff2, B>,
+  ): Free<Eff | Eff2, B>;
 
   map<B>(mapper: (value: A) => B): Free<Eff, B> {
     // return new Return_(mapper(value))
-    return this.chain((a) => new Pure(mapper(a))) as any
+    return this.chain((a) => new Pure(mapper(a))) as any;
   }
 
   handle<HandledEff>(
@@ -31,12 +31,12 @@ export abstract class Free<Eff = any, A = any> {
     if (isChained(this)) {
       for (const handler of handlers) {
         if (this.effRepr.symbol === handler.symbol) {
-          const result = handler(this.effRepr.args as any, undefined as any)
-          return this.next(result)
+          const result = handler(this.effRepr.args as any, undefined as any);
+          return this.next(result);
         }
       }
     }
-    return this as any
+    return this as any;
   }
   //   abstract map<B>(mapper: (value: A) => B): Free<Eff, B>
 }
@@ -49,31 +49,31 @@ export abstract class Free<Eff = any, A = any> {
 // export const handleExn = <T, R>(handler: (err: T) => R) =>
 //   Object.assign((err: T) => handler(err), { symbol: ExnS }) as any
 export type Handler<T, Eff> = {
-  remove: T
-  symbol: symbol
-  (args: any[], eff: Eff): any
-}
-type hoi = Exn<number, string> extends infer U | Exn<any, any> ? U : false
+  remove: T;
+  symbol: symbol;
+  (args: any[], eff: Eff): any;
+};
+type hoi = Exn<number, string> extends infer U | Exn<any, any> ? U : false;
 // type sla = Remove<Exn<number, string>, Exn<any, any>>
 function isChained(val: any): val is Chained {
-  return val instanceof Chained
+  return val instanceof Chained;
 }
 function isExn(val: Chained): val is Chained<Exn> {
-  return !!(val.effRepr.symbol === ExnS)
+  return !!(val.effRepr.symbol === ExnS);
 }
 export interface Exn<_T = any, _R = void> {
-  raise: (exn: _T) => _R
+  raise: (exn: _T) => _R;
 }
-export const ExnS = Symbol.for('@@effects/exn')
+export const ExnS = Symbol.for('@@effects/exn');
 
-export const raise = <T, R = void>(exn: T) => free<Exn<T, R>, R>(ExnS, exn)
+export const raise = <T, R = void>(exn: T) => free<Exn<T, R>, R>(ExnS, exn);
 
 // export type Remove<A, B> = A extends B ? (A extends infer U & B ? U : never) : A
 interface pure {
-  readonly pure: unique symbol
+  readonly pure: unique symbol;
 }
 interface impure {
-  readonly impure: unique symbol
+  readonly impure: unique symbol;
 }
 // export class Impure<Eff = impure, A = any> extends Free<Eff, A> {
 //     chain<B, Eff2>(chainer: (value: A) => Free<Eff2, B>): Free<Eff & Eff2, B> {
@@ -81,13 +81,13 @@ interface impure {
 //     }
 
 // }
-export class Pure<A = any> extends Free<pure, A> {
+export class Pure<Eff = pure, A = any> extends Free<Eff, A> {
   constructor(public value: A) {
-    super()
+    super();
   }
-  chain<B>(chainer: (value: A) => Free<any, B>): Free<any, B> {
-    return chainer(this.value)
-    // return new FlatMap_(this, chainer)
+
+  chain<B, Eff2>(chainer: (value: A) => Free<Eff2, B>): Free<Eff | Eff2, B> {
+    return chainer(this.value);
   }
 
   //   map<B>(mapper: (value: A) => B): Free<any, B> {
@@ -95,23 +95,21 @@ export class Pure<A = any> extends Free<pure, A> {
   //   }
 }
 interface EffectRepresentation<Args extends any[] = any[]> {
-  symbol: symbol
-  args: Args
+  symbol: symbol;
+  args: Args;
 }
 
-export class Chained<
-  Eff = any,
-  A = any,
-  EffRepr extends EffectRepresentation = EffectRepresentation,
-  B = any
-> extends Free<Eff, B> {
-  constructor(public effRepr: EffRepr, public next: (value: A) => Free<Eff, B>) {
-    super()
+export class Chained<Eff = any, A = any, B = any> extends Free<Eff, B> {
+  constructor(
+    public effRepr: EffectRepresentation,
+    public next: (value: A) => Free<Eff, B>,
+  ) {
+    super();
   }
 
   chain<C, Eff2>(chainer: (value: B) => Free<Eff2, C>): Free<Eff | Eff2, C> {
     // return this.prev.chain(this.next).chain(chainer)
-    return new Chained(this.effRepr, (val: A) => this.next(val).chain(chainer))
+    return new Chained(this.effRepr, (val: A) => this.next(val).chain(chainer));
   }
 
   //   map<C>(mapper: (value: B) => C): Free<any, C> {
@@ -121,7 +119,7 @@ export class Chained<
   //   }
 }
 export const pure = <Eff, A = any>(value: A) =>
-  (new Pure(value) as any) as Free<Eff, A>
+  (new Pure(value) as any) as Free<Eff, A>;
 
 // export const chain = <A, Eff extends Effect, Fn extends EffectCall<any>, B>(
 //   free: Fn,
@@ -136,34 +134,27 @@ export const pure = <Eff, A = any>(value: A) =>
 export const free = <Eff, A>(symbol: symbol, ...args: any[]) =>
   (new Chained(
     { symbol: symbol, args },
-    (args) => new Pure(args)
-  ) as any) as Free<Eff, A>
+    (args) => new Pure(args),
+  ) as any) as Free<Eff, A>;
 
 export type Handlers<Eff extends EasyEffect> = {
   [P in keyof Eff]: (
     ...args: Parameters<Eff[P]>
-  ) => ReturnType<Eff[P]> | Promise<ReturnType<Eff[P]>>
-}
+  ) => ReturnType<Eff[P]> | Promise<ReturnType<Eff[P]>>;
+};
 interface fn {
-  (...args: any[]): any
+  (...args: any[]): any;
 }
 // sugar syntax
 export interface EasyEffect {
-  [P: string]: fn
+  [P: string]: fn;
 }
 
 export type IsEffect<
   T,
   Prop extends string | symbol | number,
   Fn extends fn = EasyEffect[Prop extends keyof EasyEffect ? Prop : never]
-> = (
-  val: any
-) => val is Chained<
-  T,
-  any,
-  EffectRepresentation<Parameters<Fn>>,
-  ReturnType<Fn>
->
+> = (val: any) => val is Chained<T, any, ReturnType<Fn>>;
 
 export const toFree = <
   Eff extends EasyEffect,
@@ -172,12 +163,18 @@ export const toFree = <
   Args extends any[] = Parameters<Fn>,
   Return = ReturnType<Fn>
 >(): [(...args: Args) => Free<Eff, Return>, IsEffect<Eff, Prop>] => {
-  const symbol = Symbol()
+  const symbol = Symbol();
   return [
-    (...args) => new Chained({ symbol, args }, (args) => new Pure(args) as any),
-    (val): val is any => val instanceof Chained && val.effRepr.symbol === symbol
-  ]
-}
+    // (...args) => new Chained({ symbol, args }, (args) => new Pure(args)),
+    (...args) =>
+      new Chained<Eff, Return>(
+        { symbol, args },
+        (e) => new Pure<Eff, Return>(e),
+      ),
+    (val): val is any =>
+      val instanceof Chained && val.effRepr.symbol === symbol,
+  ];
+};
 
 export function doFree<
   R,
@@ -185,15 +182,15 @@ export function doFree<
   Effects = Free_ extends Free<infer E, any> ? E : never
   //   UnionEffects = UnionToIntersection<Effects>
 >(fun: () => Generator<Free_, R, any>): Free<Effects, R> {
-  const iterator = fun()
-  const state = iterator.next()
+  const iterator = fun();
+  const state = iterator.next();
   function run(state: IteratorResult<Free_, R>): any {
     if (state.done) {
-      return new Pure(state.value)
+      return new Pure(state.value);
     }
     return state.value.chain((val) => {
-      return run(iterator.next(val))
-    })
+      return run(iterator.next(val));
+    });
   }
-  return run(state)
+  return run(state);
 }
