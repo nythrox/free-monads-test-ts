95208c018a663dca61dc53d7113c8768cb15ca1d
Jason Santiago <jasonsantiagobutler@gmail.com>
2020-11-02 04:07:36 -0300

    multi shot

--- src/custom_alg_effects_impl.js
+++ src/custom_alg_effects_impl.js
@@ -1,7 +1,20 @@
-const nextImmutable = (regen, ...args) => (data) => {
-  const gen = regen(...args);
-  return gen.next(data), gen;
-};
+function clonableIterator(it, history = []) {
+  const gen = it();
+  history.forEach((v) => gen.next(v));
+  return {
+    next(arg) {
+      history.push(arg);
+      const res = gen.next(arg);
+      return res;
+    },
+    clone() {
+      return clonableIterator(it, [...history]);
+    },
+    [Symbol.iterator]() {
+      return this;
+    }
+  };
+}
 
 let debug = false;
 let handlers = Object.create(null);
@@ -9,14 +22,14 @@ let returnToDelimitedContinuation = null;
 
 function runGenerator(gen, arg, then) {
   const { value, done } = gen.next(arg);
-
+  // console.log(value);
   if (done) {
     // const _return = gen._return;
     then(value);
   } else {
     // no recursion: TLDR, if you are gonna pause it, you better play it too
-    if (typeof value === "function") {
-      value(gen, then);
+    if (value?.PAUSE) {
+      value.f(gen, then);
       return;
     }
     throw new Error("yielded invalid value: " + value.toString());
@@ -31,7 +44,7 @@ function start(gen, onDone) {
 }
 function addScopeInfo(handlers) {
   for (const p in handlers) {
-    console.log("setting", p, "to", handlers);
+    // console.log("setting", p, "to", handlers);
     handlers[p].handlerScope = handlers;
   }
 }
@@ -39,29 +52,51 @@ function addScopeInfo(handlers) {
 function* handleMulti(fn, newHandlers) {}
 
 function* handle(fn, newHandlers) {
-  return yield (lastGen, lastThen) => {
-    const thisThen = (res) => {
-      resume(lastGen, res, lastThen);
-    };
-    const currHandlers = handlers;
-    handlers = Object.assign(Object.create(handlers), newHandlers);
-    // Object.setPrototypeOf(newHandlers, handlers);
-    // handlers = newHandlers;
-    const subGen = (function* () {
+  return yield {
+    PAUSE: true,
+    f: (lastGen, lastThen) => {
+      const thisThen = (res) => {
+        resume(lastGen, res, lastThen);
+      };
+      const currHandlers = handlers;
+      const currCont = returnToDelimitedContinuation;
+      const handlersObj = Object.assign(Object.create(handlers), newHandlers);
+      addScopeInfo(handlersObj);
+      // Object.setPrototypeOf(newHandlers, handlers);
+      // handlers = newHandlers;
+      handlers = handlersObj;
       returnToDelimitedContinuation = thisThen;
-      const g = typeof fn === "function" ? fn() : fn;
-      let result = yield* g;
-      handlers = currHandlers;
-      if (newHandlers.return) {
-        const returnGen = newHandlers.return(result);
-        result = yield* returnGen;
-      }
-      return result;
-    })();
-    addScopeInfo(handlers);
-    start(subGen, thisThen);
+      const subGen = clonableIterator(
+        function* () {
+          let result = yield* fn();
+          if (newHandlers.return) {
+            const returnGen = newHandlers.return(result);
+            result = yield* returnGen;
+          }
+          return result;
+        },
+        [],
+        lastThen
+      );
+      // {
+      //   before: {
+      //     handlers: handlersObj,
+      //     returnToDelimitedContinuation: thisThen
+      //   },
+      //   after: {
+      //     handlers: currHandlers,
+      //     returnToDelimitedContinuation: currCont
+      //   }
+      // }
+      start(subGen, (val) => {
+        handlers = currHandlers;
+        returnToDelimitedContinuation = currCont;
+        thisThen(val);
+      });
+    }
   };
 }
+
 function debugPerform(name, values) {
   if (debug) {
     console.log(
@@ -72,29 +107,60 @@ function debugPerform(name, values) {
     console.log("handlers", handlers);
   }
 }
+
 function* perform(name, values) {
   debugPerform(name, values);
   // curr gen is only used to get the handlers and _then
-  const currHandlers = handlers;
-  const handler = currHandlers[name];
-  // go up once in scope (for handlers)
-  if (!handler) throw new Error("No handler found for: " + name);
-  handlers = Object.getPrototypeOf(handler.handlerScope);
-  return yield (currGen, then) => {
-    if (!handler) throw new Error("No handler for " + name);
-    const handlerGen = handler(values, function* (val) {
-      // pause handler, dont need to get gen and next because we already have it
-      return yield () => {
-        // console.log(__ === handlerGen, __undefined === returnToDelimitedContinuation);
-        // resume function depending on handler with value (val)
-        if (debug) console.log("->", val);
-        handlers = currHandlers;
-        resume(currGen, val, (handlerVal) => {
-          resume(handlerGen, handlerVal, then);
-        });
-      };
-    });
-    start(handlerGen, returnToDelimitedContinuation);
+  return yield {
+    PAUSE: true,
+    f: (currGen, then) => {
+      // if (currGen.before) {
+      //   handlers = currGen.before.handlers;
+      //   returnToDelimitedContinuation =
+      //     currGen.before.returnToDelimitedContinuation;
+      // }
+
+      const currHandlers = handlers;
+      const handler = currHandlers[name];
+      // go up once in scope (for handlers)
+      if (!handler) throw new Error("No handler found for: " + name);
+      handlers = Object.getPrototypeOf(handler.handlerScope);
+
+      const handlerGen = handler(values, function* (val) {
+        // pause handler, dont need to get gen and next because we already have it
+        return yield {
+          PAUSE: true,
+          f: () => {
+            // console.log(__ === handlerGen, __undefined === returnToDelimitedContinuation);
+            // resume function depending on handler with value (val)
+            if (debug) console.log("->", val);
+            handlers = currHandlers;
+
+            let g = currGen;
+            if (currGen.clone) {
+              g = currGen.clone();
+              //   console.log(
+              //     "has clone",
+              //     handlers,
+              //     currGen.clone().next(undefined, true)
+              //   );
+            }
+            resume(g, val, (handlerVal) => {
+              resume(handlerGen, handlerVal, (val) => {
+                // if (currGen.after) {
+                //   handlers = currGen.after.handlers;
+                //   returnToDelimitedContinuation =
+                //     currGen.after.returnToDelimitedContinuation;
+                // }
+                then(val);
+              });
+            });
+          }
+        };
+      });
+
+      start(handlerGen, returnToDelimitedContinuation);
+    }
   };
 }
 
@@ -105,6 +171,27 @@ function* pause(fn) {
     });
   };
 }
+
+// function* testMulti() {
+//   const res = yield* handle(
+//     function* () {
+//       const num = yield* perform("list", [1, 2, 3, 4]);
+//       return num + 1;
+//     },
+//     {
+//       *list(list, k) {
+//         const vals = [];
+//         for (const item of list) {
+//           vals.push(yield* k(item));
+//         }
+//         return vals;
+//       }
+//     }
+//   );
+//   console.log(res);
+// }
+
+// start(testMulti(), () => {});
 
 // function* ambTest() {
 //   const res = yield* handle(function* () {}, {
@@ -125,30 +212,32 @@ function* test2() {
   const p12 = yield* perform("plusOne", 2);
   const msg = yield* perform("name", "world");
   const p23 = yield* perform("plusOne", 3);
-  console.log("plusOne: " + p11 + p12 + p23);
-  return "plusOne: " + p11 + p12 + p23;
+  return "plusOne: " + p11 + p12 + p23 + msg;
+  // return ["10"];
+  // return p11;
 }
 
 function* test() {
-  const msg = yield* perform("name", "world");
+  // console.log("bef", handlers);
+  // const msg = yield* perform("name", "world");
+  // console.log("aft", handlers);
   const number = yield* handle(test2, {
     *return(val) {
-      return [val, yield* perform("name", "hi")];
+      // return [val, yield* perform("name", "hi")];
+      return [val];
     },
     *plusOne(num, k) {
-      const name = yield* perform("name", "notworld");
       const [res] = yield* k(num + 1);
+      const name = yield* perform("name", "notworld");
       return [res, name];
     }
   });
-  console.log("num", number);
-  // return 0;
   return number.join("");
   // return msg + "!";
 }
 
 function* hello() {
-  const res = yield* getMsg(10);
+  // const res = yield* getMsg(10);
   const res2 = yield* handle(test, {
     *return(val) {
       return val;
@@ -156,13 +245,17 @@ function* hello() {
     },
     *name(name, k) {
       const res = yield* k("Hello " + name);
-      return res;
+      const res2 = yield* k("hwwo " + name);
+      return [res + res2];
+      // return res;
     },
     *exn(exn, k) {
       return exn;
     }
   });
-  return [res, res2];
+  // return [res, res2];
+  return res2;
+  // return res;
 }
 
 function* getMsg(msg) {
