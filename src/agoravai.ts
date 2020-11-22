c8d3130ce6ddcfa115125798b849d917533e3736
Jason Santiago <jasonsantiagobutler@gmail.com>
2020-11-22 15:39:32 -0300

    uglynweird

--- src/agoravai.ts
+++ src/agoravai.ts
@@ -1,23 +1,31 @@
-type Effect<K extends PropertyKey = any, V = any, R = any> = {
+interface Effect<K extends PropertyKey = any, V = any, R = any> {
     key: K;
     value: V;
     __return: R;
-  };
-  type EffectCall<R, E extends Effect, E2 extends Effect = never> = {
+  }
+  interface EffectCall<R, E extends Effect, E2 extends Effect = never>
+    extends Syntax<R, E> {
+    _R: R;
+    _E: E;
     effect: E;
     callback: (result: E["__return"]) => Syntax<R, E2>;
     type: "effectCall";
-  };
-  type Done<R> = {
+  }
+  interface Done<R, E extends Effect> extends Syntax<R, E> {
+    _R: R;
+    _E: E;
     value: R;
     type: "done";
-  };
+  }
   // goes from A to R
-  type Handler<R, E extends Effect, A = any, E2 extends Effect = never> = {
+  interface Handler<R, E extends Effect, A = any, E2 extends Effect = never>
+    extends Syntax<R, E> {
+    _R: R;
+    _E: E;
     handle: Syntax<A, E>;
     handlers: Handlers<R, A, E, E2>;
     type: "handler";
-  };
+  }
   type Handlers<R, A, E extends Effect, E2 extends Effect = never> = {
     return: (value: A) => Syntax<R, E2> | Syntax<R, never>; // hack to make 'any is not assignable to never' work. (never is not assignable to Effect<any,any,any>)
   } & {
@@ -29,25 +37,21 @@ type Effect<K extends PropertyKey = any, V = any, R = any> = {
   
   interface Resume<V, T> extends Effect<"@@internal@@/resume", V, T> {}
   
-  type Syntax<R, E extends Effect = never> =
-    | EffectCall<R, E>
-    | Handler<R, E>
-    | Done<R>;
-  const done = <T>(value: T) => ({ value, type: "done" } as Syntax<T, never>);
+  interface Syntax<R, E extends Effect = never> {
+    _R: R;
+    _E: E;
+  }
+  const done = <T>(value: T) =>
+    (({ value, type: "done" } as any) as Syntax<T, never>);
   
   const handle = <A, E extends Effect, E2 extends Effect>(
     handle: Syntax<A, E>
   ) => <T>(handlers: Handlers<T, A, E, any>) =>
-    (({ type: "handler", handlers, handle } as any) as [
-      Syntax<T, E2>,
-      typeof handlers extends Handlers<infer _A, infer _B, infer _C, infer _E2>
-        ? E2
-        : never
-    ]);
+    (({ type: "handler", handlers, handle } as any) as Syntax<T, E2>);
   
   const performEffect = <E extends Effect>(effect: E) => <T, E2 extends Effect>(
     callback: (result: E["__return"]) => Syntax<T, E2>
-  ) => ({ type: "effectCall", callback, effect } as Syntax<T, E>);
+  ) => (({ type: "effectCall", callback, effect } as any) as Syntax<T, E>);
   
   const effect = <K extends PropertyKey, V, R>(key: K) => (value: V) =>
     ({
